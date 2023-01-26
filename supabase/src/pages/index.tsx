ab102803acd7bda7bc12d3679e0d13269279083b
oushesh <oushesh@gmail.com>
2023-01-26 17:49:19 +0400

    updated Account,Avatar and index

--- supabase/src/pages/index.tsx
+++ supabase/src/pages/index.tsx
@@ -1,8 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-
 import { Auth, ThemeSupa } from '@supabase/auth-ui-react'
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
 import Account from '../components/Account'
